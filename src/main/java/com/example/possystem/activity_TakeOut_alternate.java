30219064ec7f478d0bca5e730ea68033afad5d48
Gleb <ggladyshevsky@gmail.com>
2020-11-28 14:20:44 -0800

    menu + nav

---
+++ src/main/java/com/example/possystem/activity_TakeOut_alternate.java
@@ -0,0 +1,28 @@
+package com.example.possystem;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class activity_TakeOut_alternate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__take_out_alternate);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
